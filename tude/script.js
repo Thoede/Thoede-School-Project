6eefb6c29a84de7b35f36c40fc3a715af09fe608
Thoede <79324194+Thoede@users.noreply.github.com>
2024-08-19 13:14:14 +0930

    Delete tude directory
    wrong

--- tude/script.js
+++
@@ -1,8 +0,0 @@
-function showMoreInfo(id) {
-    var info = document.getElementById(id);
-    if (info.style.display === "none" || info.style.display === "") {
-        info.style.display = "block";
-    } else {
-        info.style.display = "none";
-    }
-}
